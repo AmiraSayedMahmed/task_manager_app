5591d14887d43096523d223392e726d55d2b0810
Amira Sayed <amirasayed1422000@gmail.com>
2024-05-19 19:23:31 +0300

    create TypeAdapter for tasksModel

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,7 +1,17 @@
-class TaskModel {
+import 'package:hive/hive.dart';
+
+//To generate Type Adapter
+part 'task_model.g.dart'; // The name of file that will be Generate
+// to generate this file ==> put this command in terminal: dart pub add dev:build_runner
+@HiveType(typeId: 0)
+class TaskModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   TaskModel(
